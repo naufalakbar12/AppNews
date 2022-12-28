6561a62f593ec3d07bd8e6c64f60884858fc5eee
naufalakbar12 <galihdwissantoso@gmail.com>
2022-12-28 13:57:53 +0700

    Lots of change Done

--- app/src/main/java/com/example/appnews/Navigation/NavigationActivity.kt
+++ app/src/main/java/com/example/appnews/Navigation/NavigationActivity.kt
@@ -4,6 +4,8 @@ import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import androidx.appcompat.app.ActionBarDrawerToggle
 import androidx.core.view.GravityCompat
+import androidx.navigation.Navigation
+import androidx.navigation.ui.NavigationUI
 import com.example.appnews.R
 import kotlinx.android.synthetic.main.activity_navigation.*
 
@@ -15,6 +17,10 @@ class NavigationActivity : AppCompatActivity() {
         setContentView(R.layout.activity_navigation)
 
         setSupportActionBar(toolbar)
+
+        val navController = Navigation.findNavController(this, R.id.fragment_container)
+        NavigationUI.setupWithNavController(nav_view, navController)
+        NavigationUI.setupActionBarWithNavController(this, navController, drawer_layout)
 
         val toggle = ActionBarDrawerToggle(this, drawer_layout, toolbar, R.string.navigation_drawer_open, R.string.navigation_drawer_close)
         drawer_layout.addDrawerListener(toggle)
