b89be073d1425a7b98c9ad9cec18f33aee229e78
naufalakbar12 <galihdwissantoso@gmail.com>
2022-12-28 13:48:45 +0700

    Lots of change Done

---
+++ app/src/main/java/com/example/appnews/Navigation/NavigationActivity.kt
@@ -0,0 +1,30 @@
+package com.example.appnews.Navigation
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import androidx.appcompat.app.ActionBarDrawerToggle
+import androidx.core.view.GravityCompat
+import com.example.appnews.R
+import kotlinx.android.synthetic.main.activity_navigation.*
+
+
+class NavigationActivity : AppCompatActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_navigation)
+
+        setSupportActionBar(toolbar)
+
+        val toggle = ActionBarDrawerToggle(this, drawer_layout, toolbar, R.string.navigation_drawer_open, R.string.navigation_drawer_close)
+        drawer_layout.addDrawerListener(toggle)
+        toggle.syncState()
+    }
+    override fun onBackPressed() {
+        if (drawer_layout.isDrawerOpen(GravityCompat.START)){
+            drawer_layout.closeDrawer(GravityCompat.START)
+        }else{
+            super.onBackPressed()
+        }
+    }
+}
